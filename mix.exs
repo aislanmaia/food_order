676c9181cd738ba371fdd3472c2e5daab1ef33e5
Aislan Maia <aislan.maia@totvs.com.br>
2024-04-06 09:05:49 -0300

    feat: add header ui

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FoodOrder.MixProject do
   def project do
     [
       app: :food_order,
-      version: "0.1.0",
+      version: "1.0.1",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
