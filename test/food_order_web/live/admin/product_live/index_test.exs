fb53601c323846c1b348f29a58e969427589a690
Aislan Maia <aislan.maia@totvs.com.br>
2024-04-24 20:39:03 -0300

    feat: create admin list products page

---
+++ test/food_order_web/live/admin/product_live/index_test.exs
@@ -0,0 +1,36 @@
+defmodule FoodOrderWeb.Admin.ProductLive.IndexTest do
+  use FoodOrderWeb.ConnCase
+  import Phoenix.LiveViewTest
+  import FoodOrder.ProductsFixtures
+
+  describe "Admin Product Index" do
+    setup [:create_product]
+
+    test "list products", %{conn: conn, product: product} do
+      {:ok, view, _html} = live(conn, ~p"/admin/products")
+
+      assert has_element?(view, "header>div>h1", "List Products")
+
+      product_id = "#products-#{product.id}"
+      assert has_element?(view, product_id)
+      assert has_element?(view, product_id <> ">td>div>span", product.name)
+
+      assert has_element?(
+               view,
+               product_id <> ">td>div>span",
+               Money.to_string(product.price)
+             )
+
+      assert has_element?(
+               view,
+               product_id <> ">td>div>span",
+               Atom.to_string(product.size)
+             )
+    end
+  end
+
+  def create_product(_) do
+    product = product_fixture()
+    %{product: product}
+  end
+end
